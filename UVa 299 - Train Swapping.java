3196c47ddd53aaadcaf860aeab2bb89ac8facf8b
Tan Li Hau <lhtan93@gmail.com>
2015-05-20 22:40:17 +0800

    UVa 299

---
+++ UVa 299 - Train Swapping.java
@@ -0,0 +1,179 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static InputReader reader;
+    static OutputWriter writer;
+
+    public static void main(String[] args){
+        reader = new InputReader(System.in);
+        writer = new OutputWriter(System.out);
+        int n = reader.nextInt();
+        int l;
+        int[] a;
+        int count = 0;
+        while(n-->0){
+            l = reader.nextInt();
+            a = new int[l];
+            for(int i=0;i<l;i++){
+                a[i] = reader.nextInt();
+            }
+            count = 0;
+            for(int i=1;i<l;i++){
+                for(int j=i-1;j>=0;j--){
+                    if(a[j+1] < a[j]){
+                        swap(a, j, j+1);
+                        count++;
+                    }else{
+                        break;
+                    }
+                }
+            }
+            writer.println("Optimal train swapping takes %d swaps.", count);
+        }
+        writer.flush();
+    }
+    static void swap(int[] a, int i, int j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream));
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+            return tokenizer.nextToken();
+        }
+        public char nextChar(){
+            try {
+                char c = '\0';
+                while(c == '\0' || c == '\n' || c == '\r')
+                    c = (char) reader.read();
+                return c;
+            }catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public String nextLine(){
+            if(tokenizer != null && tokenizer.hasMoreTokens()){
+                StringBuilder sb = new StringBuilder(tokenizer.nextToken());
+                while(tokenizer.hasMoreTokens()){
+                    sb.append(" ").append(tokenizer.nextToken());
+                }
+                return sb.toString();
+            }else{
+                try {
+                    return reader.readLine();
+                }catch(Exception e){
+                    return null;
+                }
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    static class OutputWriter {
+        public BufferedWriter writer;
+        OutputWriter(OutputStream stream) {
+            writer = new BufferedWriter(new OutputStreamWriter(stream));
+        }
+        public void print(String string){
+            try {
+                writer.write(string, 0, string.length());
+            }catch(Exception e){e.printStackTrace();}
+        }
+        public void print(char c){
+            try {
+                writer.write(c);
+            }catch(Exception e){e.printStackTrace();}
+        }
+        public void print(String format, Object... args) {
+            try {
+                String string = String.format(format, args);
+                writer.write(string, 0, string.length());
+            }catch(Exception e){e.printStackTrace();}
+        }
+        public void print(int x){
+            try {
+                String string = String.format("%d", x);
+                writer.write(string, 0, string.length());
+            }catch(Exception e){e.printStackTrace();}
+        }
+        public void print(double x){
+            try {
+                int x_int = (int) (x * 100 + 1e-6); // add 1e-6 to avoid precision error when converting to int
+                String string = String.format("%d.%02d", x_int/100, x_int%100);
+                writer.write(string, 0, string.length());
+            }catch(Exception e){e.printStackTrace();}
+        }
+        public void print(long x){
+            try {
+                String string = String.format("%d", x);
+                writer.write(string, 0, string.length());
+            }catch(Exception e){e.printStackTrace();}
+        }
+        public void println(String string){
+            print(string);
+            newline();
+        }
+        public void println(char c){
+            print(c);
+            newline();
+        }
+        public void println(String format, Object... args) {
+            print(format, args);
+            newline();
+        }
+        public void println(int x){
+            print(x);
+            newline();
+        }
+        public void println(long x){
+            print(x);
+            newline();
+        }
+        public void println(double x){
+            print(x);
+            newline();
+        }
+        public void println(){
+            newline();
+        }
+
+        private void newline() {
+            try {
+                writer.newLine();
+            }catch(Exception e){e.printStackTrace();}
+        }
+        private void flush(){
+            try {
+                writer.flush();
+            }catch(Exception e){e.printStackTrace();}
+        }
+    }
+}
